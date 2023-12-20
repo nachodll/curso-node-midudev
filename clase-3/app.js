836f99da3858b08f3e0f0f1183833bfbeea909a9
nachodll <nachodoradollamas@gmail.com>
2023-12-21 00:27:19 +0100

    beggining REST API

---
+++ clase-3/app.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const crypto = require('node:crypto')
+const movies = require('./movies.json')
+
+const app = express()
+app.use(express.json())
+app.disable('x-powered-by')
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'hola mundo' })
+})
+
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filterdMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filterdMovies)
+  }
+  return res.json(movies)
+})
+
+app.post('/movies', (req, res) => {
+  const {
+    title,
+    genre,
+    year,
+    director,
+    duration,
+    rate,
+    poster
+  } = req.body
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    genre,
+    title,
+    year,
+    director,
+    duration,
+    rate: rate ?? 0,
+    poster
+  }
+  movies.push(newMovie)
+  res.status(201).json(newMovie)
+})
+
+app.get('/movies/:id', (req, res) => { // path to regexp
+  const { id } = req.params
+  console.log(id)
+  const movie = movies.find(movie => movie.id === id)
+  console.log(movie)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log('Server listening on port http://localhost:1234')
+})
