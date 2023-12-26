c76bf389a21aa236419d228925bc1b590c60fccc
nachodll <nachodoradollamas@gmail.com>
2023-12-26 13:00:45 +0100

    web y cors

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const crypto = require('node:crypto')
+const cors = require('cors')
 const movies = require('./movies.json')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234',
+      'https://movies.com',
+      'https://midu.dev'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
 app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
@@ -68,6 +89,19 @@ app.patch('/movies/:id', (req, res) => {
 
   movies[movieIndex] = updateMovie
   return res.status(200).json(updateMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
 })
 
 const PORT = process.env.PORT ?? 1234
